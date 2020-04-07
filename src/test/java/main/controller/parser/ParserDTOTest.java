20bc23413b5470a8761c84143edc004234e713da
Anna <a.karlysheva@ukma.edu.ua>
2020-04-08 00:23:07 +0300

    parser medium test

--- src/test/java/main/controller/parser/ParserDTOTest.java
+++ src/test/java/main/controller/parser/ParserDTOTest.java
@@ -1,9 +1,11 @@
 package main.controller.parser;
 
+import main.controller.parser.dto.FileDTO;
 import main.controller.parser.dto.FolderDTO;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,7 +24,25 @@ class ParserDTOTest {
     }
 
     @Test
-    void jacksonMedium(){
+    void jacksonMedium() throws IOException {
+        FolderDTO folder = new FolderDTO();
+        folder.setPath("/root");
+        folder.setName("root");
+        FileDTO file1 = new FileDTO();
+        file1.setName("file1.txt");
+        file1.setMime_type("file/txt");
+        file1.setSize(100);
+        ArrayList<FileDTO> files = new ArrayList<>();
+        files.add(file1);
+        folder.setFiles(files);
+        ParserDTO parser = new ParserDTO();
+        FolderDTO parserFolder = parser.parse("{" +
+                "\"name\": \"root\", " +
+                "\"files\": [" +
+                "{\"name\":\"file1.txt\"," +
+                "\"mime_type\":\"file/txt\"," +
+                "\"size\":100}]}");
+        assertEquals(folder, parserFolder);
 
     }
 
